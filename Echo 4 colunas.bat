2c57dadd9354f70c5dfb5dca34f0d90cf96ce58d
ramontoshio <60797280+ramontoshio@users.noreply.github.com>
2020-02-07 18:40:12 -0300

    Add files via upload

---
+++ Echo 4 colunas.bat
@@ -0,0 +1,51 @@
+@echo off
+cls
+:menu
+cls
+color 0f
+title MUDAR IP de Rota
+mode con cols=95
+mode con lines=60
+
+echo.
+	date /t
+echo.
+echo  =============================================================================================
+echo  :                                     MARINTER TELECOM                                      :
+echo  :                             MUDAR IP de Rota Automaticamente                              :
+echo  =============================================================================================
+echo.
+echo  Escolha a ROTA:
+echo. 
+echo      -----------------       -----------------       -----------------       -----------------
+echo    1 :   AEDUARDO    :    11 :   ELISA       :    21 :   MOMBUCAF    :    31 :   PRACA5      :
+echo      -----------------       -----------------       -----------------       -----------------
+echo    2 :   ARACATIBA   :    12 :   FLAMENGO 1  :    22 :   MOMBUCA2F   :    32 :   PRACAOBP    :
+echo      -----------------       -----------------       -----------------       -----------------
+echo    3 :   BEVERLY     :    13 :   FLAMENGO 2  :    23 :   PEDREIRAS   :    33 :   PRACASMS    :
+echo      -----------------       -----------------       -----------------       -----------------
+echo    4 :   BOAVISTA    :    14 :   FLAMENGO 3  :    24 :   PELDORADOF  :    34 :   RAPHAVILLE  :
+echo      -----------------       -----------------       -----------------       -----------------
+echo    5 :   BOQUEIRAO 1 :    15 :   HVARELLA    :    25 :   PELDORADO2F :    35 :   UBATIBA 1   :
+echo      -----------------       -----------------       -----------------       -----------------
+echo    6 :   BOQUEIRAO 2 :    16 :   ITAPEBA 1   :    26 :   PIQUETE     :    36 :   UBATIBA 2   :
+echo      -----------------       -----------------       -----------------       -----------------
+echo    7 : BOSQUEITAPEBA :    17 :   ITAPEBA 2   :    27 :   PNANCY 1    :    37 :   UBATIBA 3   :
+echo      -----------------       -----------------       -----------------       -----------------
+echo    8 :   CONDADO     :    18 :   ITAPEBAF    :    28 :   PNANCY 2    :
+echo      -----------------       -----------------       -----------------
+echo    9 :   CORDEIRINHO :    19 :   MARQUES     :    29 :   PNANCY 3    :
+echo      -----------------       -----------------       -----------------
+echo   10 :   DIVINEIA    :    20 :   METROPOLE   :    30 :   PNEGRA      :
+echo      -----------------       -----------------       -----------------
+echo.
+echo.
+echo     ---------------------
+echo  100:     VER O MAC     :
+echo     ---------------------
+echo  200:     VER O IP      :
+echo     ---------------------
+echo  300:   IP AUTOMATICO   :
+echo     ---------------------
+echo  400:       SAIR        :
+echo     ---------------------
